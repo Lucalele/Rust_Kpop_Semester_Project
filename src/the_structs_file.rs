ededa30fd6aebb4072aad1e18f8271de9f82f9ef
Luca <153230751+Lucalele@users.noreply.github.com>
2026-06-17 10:51:57 -0400

    right now all the structs are in 1 file, don't ask

--- src/the_structs_file.rs
+++ src/the_structs_file.rs
@@ -1,9 +1,9 @@
 pub struct Album {
     pub album_id: u32,
     pub title: String,
-    pub artist: String,
+    pub artist_id: u32,
     pub release_date: NaiveDate,
-    pub language: String,
+    pub language: Language,
     pub versions: Vec<String>,
 }
 
@@ -11,23 +11,44 @@ pub struct Group{
     pub group_id: u32,
     pub group_name: String,
     pub debut_date: NaiveDate,
-    pub gender: u8, //set male to 0, female to 1, co-ed to 2
+    pub gender: GroupGender, //set male to 0, female to 1, co-ed to 2
     pub comapany_name: Vec<String>,
     pub label_name: Vec<String>,
 }
 
 pub struct Idol{
     pub idol_id: u32,
-    pub idol_gender: u8, //0 for male, 1 for female
+    pub idol_gender: Gender, //0 for male, 1 for female
 }
 
-pub struct IdolName{
+pub struct IdolName {
+    pub idol_name_id: u32,
     pub idol_id: u32,
-    pub name: String,
-    pub language: u8, // 0 = Hangul, 1 = Latin, 2 = Japanese, 3 = Mandarin
+    pub display_name: String,
+    pub language: Language,
+    pub name_type: NameType,
 }
 
+pub struct NameAlias {
+    pub idol_name_id: u32,
+    pub alias: String,
+}
 
+pub enum GroupGender {
+    Male,
+    Female,
+    CoEd,
+}
 
+pub enum Gender {
+    Male,
+    Female,
+}
 
+pub enum Language {
+    Hangul,
+    Latin,
+    Japanese,
+    Mandarin,
+}
 
