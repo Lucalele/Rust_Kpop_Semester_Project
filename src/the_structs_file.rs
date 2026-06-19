b20724292744082892fad0abe4f31e5035630a1e
Luca <153230751+Lucalele@users.noreply.github.com>
2026-06-19 14:42:24 -0400

    right now all the structs are in 1 file, don't ask

--- src/the_structs_file.rs
+++ src/the_structs_file.rs
@@ -4,22 +4,38 @@ pub struct Album {
     pub artist_id: u32,
     pub release_date: NaiveDate,
     pub language: Language,
-    pub versions: Vec<String>,
+    pub versions: Vec<String>, //THIS WILL BE COLLAPSED INTO A SINGLE OPTION
 }
 
 pub struct Group{
     pub group_id: u32,
     pub group_name: String,
     pub debut_date: NaiveDate,
-    pub gender: GroupGender, //set male to 0, female to 1, co-ed to 2
-    pub comapany_name: Vec<String>,
-    pub label_name: Vec<String>,
+    pub gender: GroupGender, 
+    pub company_ids: Vec<u32>,
+    pub label_ids: Vec<u32>,
 }
 
 pub struct Idol{
     pub idol_id: u32,
-    pub idol_gender: Gender, //0 for male, 1 for female
+
+    //NOT THE SAME AS GROUP GENDER
+    pub idol_gender: Gender,
+
+    // if 0, soloist
+    pub group_id: Vec<u32>,
+
+    // most idols are not in subunits
+    pub subunit_id: Vec<u32>,
+
+    // 0 = indie
+    pub company_id: Vec<u32>,
+
+    // 0 = indie or company has no labels
+    pub label_id: Vec<u32>,
 }
+
+
 
 pub struct IdolName {
     pub idol_name_id: u32,
@@ -30,7 +46,7 @@ pub struct IdolName {
 }
 
 pub struct NameAlias {
-    pub idol_name_id: u32,
+    pub idol_alias_id: u32,
     pub alias: String,
 }
 
@@ -52,3 +68,25 @@ pub enum Language {
     Mandarin,
 }
 
+pub struct Label {
+    pub label_id: u32,
+    pub label_name: String,
+}
+
+pub struct Company{
+    pub company_id: u32,
+    pub company_name: String,
+}
+
+pub struct Subunit {
+    pub subunit_id: u32,
+    pub subunit_name: String,
+
+    // A subunit always belongs to exactly one parent group
+    pub parent_group_id: u32,
+
+    pub debut_date: NaiveDate,
+
+    // Same enum as GroupGender
+    pub gender: GroupGender,
+}
