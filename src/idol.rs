8d5fd9d19718edcd08c8dd29b155080f284d56ee
Luca <153230751+Lucalele@users.noreply.github.com>
2026-07-09 16:21:20 -0400

    diesel

--- src/idol.rs
+++ src/idol.rs
@@ -1,23 +1,28 @@
 use crate::enums::Language;
 
-pub struct Idol{
+pub struct Idol {
     pub idol_id: u32,
 
-    //NOT THE SAME AS GROUP GENDER
+    // NOT the same as group gender
     pub idol_gender: Gender,
 
-    // if 0, soloist
-    pub group_id: Vec<u32>,
+    // Groups the idol is/was in
+    pub group_ids: Vec<u32>,
 
-    // most idols are not in subunits
-    pub subunit_id: Vec<u32>,
+    // Subunits the idol is/was in
+    pub subunit_ids: Vec<u32>,
 
-    // 0 = indie
-    pub company_id: Vec<u32>,
+    // Project groups/collabs the idol is/was in
+    pub project_group_ids: Vec<u32>,
 
-    // 0 = indie or company has no labels
-    pub label_id: Vec<u32>,
+    // If true, they can have solo albums
+    pub is_soloist: bool,
+
+    // Optional, mostly for filtering
+    pub company_ids: Vec<u32>,
+    pub label_ids: Vec<u32>,
 }
+
 pub struct IdolName {
     pub idol_name_id: u32,
     pub idol_id: u32,
@@ -28,8 +33,10 @@ pub struct IdolName {
 
 pub struct NameAlias {
     pub idol_alias_id: u32,
+    pub idol_id: u32,
     pub alias: String,
 }
+
 pub enum Gender {
     Male,
     Female,
@@ -43,4 +50,3 @@ pub enum NameType {
     ChineseName,
     Nickname,
 }
-
