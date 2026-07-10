b2d0d15e54c3b18a2fc9a2d27c0dbf455b64a6ec
Luca <153230751+Lucalele@users.noreply.github.com>
2026-07-10 13:16:33 -0400

    read.me

--- src/groups.rs
+++ src/groups.rs
@@ -3,7 +3,7 @@ use chrono::NaiveDate;
 
 pub struct IdolGroup{
     pub group_id: u32,
-    pub group_name: String,
+    pub group_name: String, 
     pub debut_date: NaiveDate,
     pub gender: GroupGender, 
     pub company_ids: Vec<u32>,
@@ -26,6 +26,7 @@ pub struct Subunit {
 pub struct ProjectGroup {
     pub project_group_id: u32,
     pub project_group_name: String,
+    pub parent_group_ids: Vec<u32>,
     pub debut_date: NaiveDate,
     pub gender: GroupGender,
 }
