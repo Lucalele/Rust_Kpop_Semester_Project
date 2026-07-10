fecfe4fe0b1e75d1cbc76a2e8597be8f99b14aae
Luca <153230751+Lucalele@users.noreply.github.com>
2026-07-10 12:27:15 -0400

    diesel

--- src/companies.rs
+++ src/companies.rs
@@ -1,9 +1,11 @@
 pub struct Label {
     pub label_id: u32,
     pub label_name: String,
+    pub company_id: u32,
 }
 
 pub struct Company{
     pub company_id: u32,
     pub company_name: String,
+    
 }
