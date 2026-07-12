38b236ca5f444379c0751a7bd6c6c179fde48b81
Luca <153230751+Lucalele@users.noreply.github.com>
2026-07-12 16:23:04 -0400

    rotations

--- src/database.rs
+++ src/database.rs
@@ -13,3 +13,59 @@ pub fn establish_connection() -> SqliteConnection {
         panic!("Could not connect to {database_url}: {error}");
     })
 }
+
+pub fn establish_database_1_connection() -> SqliteConnection {
+    let database_url = "database1.sqlite";
+
+    SqliteConnection::establish(database_url).unwrap_or_else(|error| {
+        panic!("Could not connect to {database_url}: {error}");
+    })
+}
+
+pub fn establish_database_2_connection() -> SqliteConnection {
+    let database_url = "database2.sqlite";
+
+    SqliteConnection::establish(database_url).unwrap_or_else(|error| {
+        panic!("Could not connect to {database_url}: {error}");
+    })
+}
+
+pub fn establish_database_3_connection() -> SqliteConnection {
+    let database_url = "database3.sqlite";
+
+    SqliteConnection::establish(database_url).unwrap_or_else(|error| {
+        panic!("Could not connect to {database_url}: {error}");
+    })
+}
+
+pub fn establish_database_4_connection() -> SqliteConnection {
+    let database_url = "database4.sqlite";
+
+    SqliteConnection::establish(database_url).unwrap_or_else(|error| {
+        panic!("Could not connect to {database_url}: {error}");
+    })
+}
+
+pub fn establish_database_5_connection() -> SqliteConnection {
+    let database_url = "database5.sqlite";
+
+    SqliteConnection::establish(database_url).unwrap_or_else(|error| {
+        panic!("Could not connect to {database_url}: {error}");
+    })
+}
+
+pub fn establish_database_6_connection() -> SqliteConnection {
+    let database_url = "database6.sqlite";
+
+    SqliteConnection::establish(database_url).unwrap_or_else(|error| {
+        panic!("Could not connect to {database_url}: {error}");
+    })
+}
+
+pub fn establish_database_7_connection() -> SqliteConnection {
+    let database_url = "database7.sqlite";
+
+    SqliteConnection::establish(database_url).unwrap_or_else(|error| {
+        panic!("Could not connect to {database_url}: {error}");
+    })
+}
