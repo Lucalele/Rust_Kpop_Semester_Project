30227fa62e78f39635b3261c333481eaf93e0f34
Luca <153230751+Lucalele@users.noreply.github.com>
2026-07-21 10:32:16 -0400

    tape deck

--- src/randomizer.rs
+++ src/randomizer.rs
@@ -1,33 +1,40 @@
-fn random_album() {
+use diesel::dsl::sql;
+use diesel::prelude::*;
+use diesel::sql_types::Integer;
+use diesel::sqlite::SqliteConnection;
 
+use crate::album::Album;
+use crate::schema::albums;
 
+pub fn random_album(
+    connection: &mut SqliteConnection,
+    amount: i64,
+) -> QueryResult<Vec<Album>> {
+    albums::table
+        .select(Album::as_select())
+        .order(sql::<Integer>("RANDOM()"))
+        .limit(amount)
+        .load(connection)
 }
 
-fn random_artist() {
-
+pub fn random_artist(
+    connection: &mut SqliteConnection,
+    selected_artist_id: i32,
+    amount: i64,
+) -> QueryResult<Vec<Album>> {
+    albums::table
+        .filter(albums::artist_id.eq(selected_artist_id))
+        .select(Album::as_select())
+        .order(sql::<Integer>("RANDOM()"))
+        .limit(amount)
+        .load(connection)
 }
 
-fn random_group() {
-
+pub fn shuffle(
+    connection: &mut SqliteConnection,
+) -> QueryResult<Vec<Album>> {
+    albums::table
+        .select(Album::as_select())
+        .order(sql::<Integer>("RANDOM()"))
+        .load(connection)
 }
-
-fn random_subunit() {
-
-}
-
-fn random_project_group(){
-
-}
-
-fn random_company() {
-
-}
-
-fn random_label() {
-    
-}
-
-fn random_member() {
-
-}
-
