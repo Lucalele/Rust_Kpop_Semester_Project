a797364df3a3a33e1eb4fedb1f5f98ba58af4c15
Luca <153230751+Lucalele@users.noreply.github.com>
2026-07-24 12:28:55 -0400

    inserts

--- src/idol.rs
+++ src/idol.rs
@@ -18,6 +18,40 @@ pub struct Idol {
     pub is_soloist: Option<bool>,
 }
 
+#[derive(Debug, Insertable)]
+#[diesel(table_name = idols)]
+pub struct NewIdol<'a> {
+    pub idol_gender: &'a str,
+    pub is_soloist: Option<bool>,
+}
+
+
+
+pub fn insert_idol(
+    connection: &mut SqliteConnection,
+    idol_gender: &str,
+    is_soloist: Option<bool>,
+) -> QueryResult<usize> {
+    let new_idol = NewIdol {
+        idol_gender,
+        is_soloist,
+    };
+
+    diesel::insert_into(idols::table)
+        .values(&new_idol)
+        .execute(connection)
+}
+
+pub fn load_idols(
+    connection: &mut SqliteConnection,
+) -> QueryResult<Vec<Idol>> {
+    idols::table
+        .select(Idol::as_select())
+        .order(idols::idol_id.asc())
+        .load(connection)
+}
+
+
 #[derive(Debug, Queryable, Selectable, Identifiable, Associations)]
 #[diesel(table_name = idol_names)]
 #[diesel(primary_key(idol_name_id))]
@@ -33,6 +67,36 @@ pub struct IdolName {
     pub name: String,
 }
 
+//possibly unused
+#[derive(Debug, Insertable)]
+#[diesel(table_name = idol_names)]
+pub struct NewIdolName<'a> {
+    pub idol_id: i32,
+    pub name: &'a str,
+}
+
+pub fn insert_idol_name(
+    connection: &mut SqliteConnection,
+    idol_id_value: i32,
+    idol_name_value: &str,
+) -> QueryResult<usize> {
+    diesel::insert_into(idol_names::table)
+        .values((
+            idol_names::idol_id.eq(idol_id_value),
+            idol_names::name.eq(idol_name_value),
+        ))
+        .execute(connection)
+}
+
+pub fn load_idol_names(
+    connection: &mut SqliteConnection,
+) -> QueryResult<Vec<IdolName>> {
+    idol_names::table
+        .select(IdolName::as_select())
+        .order(idol_names::idol_name_id.asc())
+        .load(connection)
+}
+
 #[derive(Debug, Queryable, Selectable, Identifiable)]
 #[diesel(table_name = idol_group_memberships)]
 #[diesel(primary_key(idol_id, group_id))]
@@ -41,12 +105,58 @@ pub struct IdolGroupMembership {
     pub group_id: i32,
 }
 
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = idol_group_memberships)]
+pub struct NewIdolGroupMembership {
+    pub idol_id: i32,
+    pub group_id: i32,
+}
+
+pub fn insert_idol_group_membership(
+    connection: &mut SqliteConnection,
+    idol_id: i32,
+    group_id: i32,
+) -> QueryResult<usize> {
+    let membership = NewIdolGroupMembership {
+        idol_id,
+        group_id,
+    };
+
+    diesel::insert_into(idol_group_memberships::table)
+        .values(&membership)
+        .execute(connection)
+}
+
+
 #[derive(Debug, Queryable, Selectable, Identifiable)]
 #[diesel(table_name = idol_subunit_memberships)]
 #[diesel(primary_key(idol_id, subunit_id))]
 pub struct IdolSubunitMembership {
     pub idol_id: i32,
     pub subunit_id: i32,
+}
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = idol_subunit_memberships)]
+pub struct NewIdolSubunitMembership {
+    pub idol_id: i32,
+    pub subunit_id: i32,
+}
+
+pub fn insert_idol_subunit_membership(
+    connection: &mut SqliteConnection,
+    idol_id: i32,
+    subunit_id: i32,
+) -> QueryResult<usize> {
+    let membership = NewIdolSubunitMembership {
+        idol_id,
+        subunit_id,
+    };
+
+    diesel::insert_into(idol_subunit_memberships::table)
+        .values(&membership)
+        .execute(connection)
 }
 
 #[derive(Debug, Queryable, Selectable, Identifiable)]
@@ -57,6 +167,29 @@ pub struct IdolProjectGroupMembership {
     pub project_group_id: i32,
 }
 
+#[derive(Debug, Insertable)]
+#[diesel(table_name = idol_project_group_memberships)]
+pub struct NewIdolProjectGroupMembership {
+    pub idol_id: i32,
+    pub project_group_id: i32,
+}
+
+pub fn insert_idol_project_group_membership(
+    connection: &mut SqliteConnection,
+    idol_id: i32,
+    project_group_id: i32,
+) -> QueryResult<usize> {
+    let membership = NewIdolProjectGroupMembership {
+        idol_id,
+        project_group_id,
+    };
+
+    diesel::insert_into(idol_project_group_memberships::table)
+        .values(&membership)
+        .execute(connection)
+}
+
+
 #[derive(Debug, Queryable, Selectable, Identifiable)]
 #[diesel(table_name = idol_companies)]
 #[diesel(primary_key(idol_id, company_id))]
@@ -64,6 +197,29 @@ pub struct IdolCompany {
     pub idol_id: i32,
     pub company_id: i32,
 }
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = idol_companies)]
+pub struct NewIdolCompany {
+    pub idol_id: i32,
+    pub company_id: i32,
+}
+
+pub fn insert_idol_company(
+    connection: &mut SqliteConnection,
+    idol_id: i32,
+    company_id: i32,
+) -> QueryResult<usize> {
+    let relationship = NewIdolCompany {
+        idol_id,
+        company_id,
+    };
+
+    diesel::insert_into(idol_companies::table)
+        .values(&relationship)
+        .execute(connection)
+}
+
 
 #[derive(Debug, Queryable, Selectable, Identifiable)]
 #[diesel(table_name = idol_labels)]
@@ -74,15 +230,25 @@ pub struct IdolLabel {
 }
 
 #[derive(Debug, Insertable)]
-#[diesel(table_name = idols)]
-pub struct NewIdol<'a> {
-    pub idol_gender: &'a str,
-    pub is_soloist: Option<bool>,
+#[diesel(table_name = idol_labels)]
+pub struct NewIdolLabel {
+    pub idol_id: i32,
+    pub label_id: i32,
 }
 
-#[derive(Debug, Insertable)]
-#[diesel(table_name = idol_names)]
-pub struct NewIdolName<'a> {
-    pub idol_id: i32,
-    pub name: &'a str,
+pub fn insert_idol_label(
+    connection: &mut SqliteConnection,
+    idol_id: i32,
+    label_id: i32,
+) -> QueryResult<usize> {
+    let relationship = NewIdolLabel {
+        idol_id,
+        label_id,
+    };
+
+    diesel::insert_into(idol_labels::table)
+        .values(&relationship)
+        .execute(connection)
 }
+
+
