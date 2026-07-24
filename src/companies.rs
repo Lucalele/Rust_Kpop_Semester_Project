a0953a6b1b4b6e1e02c070c45807eaecde590e15
Luca <153230751+Lucalele@users.noreply.github.com>
2026-07-24 12:20:51 -0400

    inserts

--- src/companies.rs
+++ src/companies.rs
@@ -16,6 +16,27 @@ pub struct NewCompany<'a> {
     pub company_name: &'a str,
 }
 
+
+pub fn insert_company(
+    connection: &mut SqliteConnection,
+    company_name: &str,
+) -> QueryResult<usize> {
+    let new_company = NewCompany { company_name };
+
+    diesel::insert_into(companies::table)
+        .values(&new_company)
+        .execute(connection)
+}
+
+pub fn load_companies(
+    connection: &mut SqliteConnection,
+) -> QueryResult<Vec<Company>> {
+    companies::table
+        .select(Company::as_select())
+        .order(companies::company_id.asc())
+        .load(connection)
+}
+
 #[derive(Debug, Queryable, Selectable, Identifiable, Associations)]
 #[diesel(table_name = labels)]
 #[diesel(primary_key(label_id))]
@@ -31,4 +52,28 @@ pub struct Label {
 pub struct NewLabel<'a> {
     pub label_name: &'a str,
     pub company_id: i32,
+}
+
+pub fn insert_label(
+    connection: &mut SqliteConnection,
+    label_name: &str,
+    company_id: i32,
+) -> QueryResult<usize> {
+    let new_label = NewLabel {
+        label_name,
+        company_id,
+    };
+
+    diesel::insert_into(labels::table)
+        .values(&new_label)
+        .execute(connection)
+}
+
+pub fn load_labels(
+    connection: &mut SqliteConnection,
+) -> QueryResult<Vec<Label>> {
+    labels::table
+        .select(Label::as_select())
+        .order(labels::label_id.asc())
+        .load(connection)
 }
